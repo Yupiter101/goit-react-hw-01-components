a61002b0fbda7d3cebe6437fae2837980da7d5ab
Sergey_Sanyuk <shv08@ukr.net>
2023-05-29 16:15:41 +0300

    V11
    ...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import { Profile } from "./Profile/Profile";
 import { Statistic } from "./Statistic/Statistic";
 import { FriendList } from "./FriendList/FriendList";
 import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
-import user from "../data/user.json";
-import data from "../data/data.json";
-import friends from "../data/friends.json";
-import transactions from '../data/transactions.json';
+import user from "../Data/user.json";
+import data from "../Data/data.json";
+import friends from "../Data/friends.json";
+import transactions from '../Data/transactions.json';
 
 // console.log(data);
 
